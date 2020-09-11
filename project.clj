3616ff82916f597e59075267ec197f0a74b1897e
Ivan Grishaev <ivan@grishaev.me>
2020-09-11 15:52:16 +0300

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject remus "0.2.0-SNAPSHOT"
+(defproject remus "0.2.0"
   :description "Attentive RSS/Atom feed parser"
 
   :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
